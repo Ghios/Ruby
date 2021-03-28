e60ed249453632e775d2f08104720c2feba7b84c
”Ghios” <lubomirshava@gmail.com>
2021-03-28 13:09:30 +0300

    add 0ne-to many assoc

--- Unit 6/alpha-blog/app/models/article.rb
+++ Unit 6/alpha-blog/app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :tile, presence: true, length: { minimum: 6, maximum: 100}
     validates :description, presence: true
 end
--- Unit 6/alpha-blog/db/schema.rb
+++ Unit 6/alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_090027) do
+ActiveRecord::Schema.define(version: 2021_03_28_094949) do
 
   create_table "articles", force: :cascade do |t|
     t.string "tile"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_28_090027) do
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
